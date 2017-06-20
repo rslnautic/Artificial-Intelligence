6b3f89b801c9cf305052cf74c340ac61627f90dd
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-06-20 14:43:33 +0200

    Working on generateFStar

--- StripsPathFindingIA/Assets/Scripts/cegimo-raselo-seromo/Minds/PathfindingMind.cs
+++ StripsPathFindingIA/Assets/Scripts/cegimo-raselo-seromo/Minds/PathfindingMind.cs
@@ -10,23 +10,44 @@ public class PathfindingMind : IMind {
 
     private Move move;
 
+    private float[,] fstar = null;
+
     public PathfindingMind()
     {
         _pathfinding = new PathFinding();
+
+        //generation of heuristic distances
+        //From every node to the end (top tight)
     }
 
     public Move.MoveDirection GetNextMove(Vector2 currentPos, GenerateMap map)
     {
-        if (_resultado == null){
+        if (fstar == null){
+            fstar = GenerateFStar(map);
+        }
+        if (_resultado == null) {
             /*_resultado = _pathfinding.Buscar(new Estado(currentPos, map, Move.MoveDirection.Right));
             return _resultado.Estado.Accion;*/
         }
-        else{
+        else {
             /*_resultado = _resultado.Padre;
             return _resultado.Estado.Accion;*/
         }
 
         //PLACEHOLDER
         return Move.MoveDirection.Right;
+    }
+
+    public float[,] GenerateFStar(GenerateMap map)
+    {
+
+        fstar = new float[map.cols, map.rows];
+
+        foreach (GenerateMap.TileType tile in map.GeneratedMap)
+        {
+
+        }
+
+        return fstar;
     }
 }
